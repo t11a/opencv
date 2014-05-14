b2a283a3cdef1f5d40423181cf7f12deea0b1f2d
tsuyoshi.ogawa <mobile.chime@gmail.com>
2014-05-15 00:31:06 +0900

    1st commit

---
+++ video_sample/video_sample/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  video_sample
+//
+//  Created by Tsuyoshi Ogawa on 5/14/14.
+//  Copyright (c) 2014 Tsuyoshi Ogawa. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    VideoCapture cap(0);
+    
+    Mat frame;
+    
+    while (waitKey(1) != 'q') {
+        cap >> frame;
+        imshow("sample", frame);
+    }
+    return 0;
+}
+
